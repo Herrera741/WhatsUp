83081a2a4238fc0812ceee661cddb0791f4b9458
Sergio <sherrera725@gmail.com>
2023-05-16 22:56:32 -0700

    Initial Commit

---
+++ WhatsUp/WhatsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpApp.swift
+//  WhatsUp
+//
+//  Created by Sergio Herrera on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
